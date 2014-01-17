e6472f52ec180e65b4134e42f9247cc5b2556cd6
John-John Tedro <johnjohn.tedro@gmail.com>
2014-01-17 22:26:11 +0100

    evc: Initial framework

--- evd.gemspec
+++ evd.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "ripl"
   spec.add_dependency "eventmachine"
   spec.add_dependency "riemann-client"
 
--- lib/evc.rb
+++ lib/evc.rb
@@ -1,8 +1,23 @@
+require 'ripl'
 require 'optparse'
+require 'eventmachine'
 
 module EVC
+  class TraceConnection < EM::Connection
+    include EM::Protocols::LineText2
+
+    def unbind
+      EM.stop
+    end
+
+    def receive_line line
+      d = JSON.load line
+      p d
+    end
+  end
+
   def self.opts
-    @@opts ||= {:debug => false}
+    @@opts ||= {:debug => false, :host => "localhost", :port => 9999}
   end
 
   def self.parser
@@ -19,8 +34,24 @@ module EVC
     parser.parse args
   end
 
+  class Context
+    def initialize opts
+      @host = opts[:host]
+      @port = opts[:port]
+    end
+
+    def trace
+      EM.run do
+        EM.connect(@host, @port, EVC::TraceConnection)
+      end
+    end
+  end
+
   def self.main(args)
     parse_options args
-    puts opts
+
+    Context.new(opts).instance_eval do
+      Ripl.start :binding => binding
+    end
   end
 end
