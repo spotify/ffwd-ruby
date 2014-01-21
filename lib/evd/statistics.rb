d1801a965fe16a2558c4ceb0af449459286eca44
John-John Tedro <udoprog@spotify.com>
2014-01-21 15:23:33 +0100

    core: Implement memory limitation.

--- lib/evd/statistics.rb
+++ lib/evd/statistics.rb
@@ -3,14 +3,23 @@ require_relative 'statistics/system_statistics'
 module EVD
   module Statistics
     class Collector
-      def initialize emitter, opts={}
+      #
+      # Initialize the statistics collector.
+      # emitter - The emitter used to dispatch metrics for all reporters and
+      # statistics collectors.
+      # system_channel - A side-channel used by the SystemStatistics component
+      # to report information about the system. Messages sent on this channel
+      # help Core decide if it should seppuku.
+      def initialize emitter, system_channel, opts={}
         @emitter = emitter
         @period = opts[:period] || 1
         @tags = opts[:tags] || []
         @attributes = opts[:attributes] || {}
         @reporters = {}
 
-        if (system = SystemStatistics.new(opts[:system] || {})).check
+        system = SystemStatistics.new(system_channel, opts[:system] || {})
+
+        if system.check
           @system = system
         else
           @system = nil
@@ -54,8 +63,8 @@ module EVD
       end
     end
 
-    def self.setup emitter, opts={}
-      Collector.new emitter, opts
+    def self.setup emitter, system_channel, opts={}
+      Collector.new emitter, system_channel, opts
     end
   end
 end
