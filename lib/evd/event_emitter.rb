4051dab4316fae5f695f12ee96ec4cfb057470cf
John-John Tedro <udoprog@spotify.com>
2014-01-22 17:03:37 +0100

    metric/event_emitter: Remove dead code and make sure all metadata is configurable.

--- lib/evd/event_emitter.rb
+++ lib/evd/event_emitter.rb
@@ -1,12 +1,16 @@
-require_relative 'logging'
 require_relative 'utils'
 require_relative 'event'
 
 module EVD
+  # Used to emit events to an 'output' channel
+  #
+  # Can take two parts of a configuration 'base' and 'opts' to decide which
+  # metadata emitted events should be decorated with.
   class EventEmitter
     def initialize output, base, opts
       @output = output
       @host = opts[:host] || base[:host] || EVD.current_host
+      @ttl = opts[:ttl] || base[:ttl]
       @tags = EVD.merge_sets base[:tags], opts[:tags]
       @attributes = EVD.merge_hashes base[:attributes], opts[:attributes]
     end
