e2c3fa2320420d31874b713e8861fb4903dc17be
John-John Tedro <udoprog@spotify.com>
2014-02-13 20:25:19 +0100

    debug: Split out TCP component.

--- lib/ffwd/debug.rb
+++ lib/ffwd/debug.rb
@@ -7,8 +7,7 @@ require_relative 'metric'
 require_relative 'retrier'
 require_relative 'lifecycle'
 
-require_relative 'debug/connection'
-require_relative 'debug/monitor_session'
+require_relative 'debug/tcp'
 
 module FFWD::Debug
   module Input
@@ -40,66 +39,6 @@ module FFWD::Debug
 
     def self.serialize_metric metric
       metric.to_h
-    end
-  end
-
-  class TCP
-    include FFWD::Logging
-    include FFWD::Lifecycle
-
-    def initialize host, port, rebind_timeout
-      @clients = {}
-      @sessions = {}
-      @host = host
-      @port = port
-      @peer = "#{@host}:#{@port}"
-
-      r = FFWD::Retrier.new(log, self, rebind_timeout) do |attempt|
-        EM.start_server @host, @port, Connection, self
-        log.info "Bind on tcp://#{@peer} (attempt #{attempt})"
-      end
-
-      r.error do |attempt, timeout, e|
-        log.error "Failed to bind tcp://#{@peer} (attempt #{attempt}), retry in #{timeout}s", e
-      end
-    end
-
-    def register_client peer, client
-      @sessions.each do |id, session|
-        session.register peer, client
-      end
-
-      @clients[peer] = client
-    end
-
-    def unregister_client peer, client
-      @sessions.each do |id, session|
-        session.unregister peer, client
-      end
-
-      @clients.delete peer
-    end
-
-    # Setup monitor hooks for the specified input and output channel.
-    def monitor id, channel, type
-      if session = @sessions[id]
-        log.error "Session already monitored: #{id}"
-        return
-      end
-
-      session = @sessions[id] = MonitorSession.new id, channel, type
-
-      # provide the session to the already connected clients.
-      @clients.each do |peer, client|
-        session.register peer, client
-      end
-    end
-
-    def unmonitor id
-      if session = @sessions[id]
-        session.stop
-        @sessions.delete id
-      end
     end
   end
 
---
+++ lib/ffwd/debug/tcp.rb
@@ -0,0 +1,68 @@
+require_relative '../lifecycle'
+require_relative '../logging'
+require_relative '../retrier'
+
+require_relative 'connection'
+require_relative 'monitor_session'
+
+module FFWD::Debug
+  class TCP
+    include FFWD::Logging
+    include FFWD::Lifecycle
+
+    def initialize host, port, rebind_timeout
+      @clients = {}
+      @sessions = {}
+      @host = host
+      @port = port
+      @peer = "#{@host}:#{@port}"
+
+      r = FFWD::Retrier.new(log, self, rebind_timeout) do |attempt|
+        EM.start_server @host, @port, Connection, self
+        log.info "Bind on tcp://#{@peer} (attempt #{attempt})"
+      end
+
+      r.error do |attempt, timeout, e|
+        log.error "Failed to bind tcp://#{@peer} (attempt #{attempt}), retry in #{timeout}s", e
+      end
+    end
+
+    def register_client peer, client
+      @sessions.each do |id, session|
+        session.register peer, client
+      end
+
+      @clients[peer] = client
+    end
+
+    def unregister_client peer, client
+      @sessions.each do |id, session|
+        session.unregister peer, client
+      end
+
+      @clients.delete peer
+    end
+
+    # Setup monitor hooks for the specified input and output channel.
+    def monitor id, channel, type
+      if session = @sessions[id]
+        log.error "Session already monitored: #{id}"
+        return
+      end
+
+      session = @sessions[id] = MonitorSession.new id, channel, type
+
+      # provide the session to the already connected clients.
+      @clients.each do |peer, client|
+        session.register peer, client
+      end
+    end
+
+    def unmonitor id
+      if session = @sessions[id]
+        session.stop
+        @sessions.delete id
+      end
+    end
+  end
+end
