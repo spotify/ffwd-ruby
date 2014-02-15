68f7db6755157b5212bbc18c638e2bc5ade93b31
John-John Tedro <johnjohn.tedro@gmail.com>
2014-02-15 21:12:36 +0100

    release 0.0.3

--- lib/ffwd/version.rb
+++ lib/ffwd/version.rb
@@ -1,3 +1,3 @@
 module FFWD
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
--- plugins/ffwd-tunnel/lib/ffwd/plugin/tunnel/version.rb
+++ plugins/ffwd-tunnel/lib/ffwd/plugin/tunnel/version.rb
@@ -1,7 +1,7 @@
 module FFWD
   module Plugin
     module Tunnel
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
     end
   end
 end
