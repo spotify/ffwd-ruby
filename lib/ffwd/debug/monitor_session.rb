spotify/ffwd-ruby

commit 129d983012dc4af27748d3b8b51b90ffb16ddb48

Bootup and shutdown using lifecycle dependencies.

lib/ffwd/debug/monitor_session.rb
@@ -2,18 +2,16 @@ require_relative '../lifecycle'
 
 module FFWD::Debug
   class MonitorSession
-    include FFWD::Lifecycle
-
     attr_reader :id
 
     def initialize id, channel, type
-      @id = id
-      @channel = channel
       @type = type
       @clients = {}
 
-      starting do
-        event_sub = @channel.event_subscribe do |event|
+      subs = []
+
+      channel.starting do
+        subs << channel.event_subscribe do |event|
           data = @type.serialize_event event
 
           begin
@@ -24,7 +22,7 @@ module FFWD::Debug
           end
         end
 
-        metric_sub = @channel.metric_subscribe do |metric|
+        subs << channel.metric_subscribe do |metric|
           data = @type.serialize_metric metric
 
           begin
@@ -34,11 +32,10 @@ module FFWD::Debug
             return
           end
         end
+      end
 
-        stopping do
-          @channel.event_unsubscribe event_sub
-          @channel.metric_unsubscribe metric_sub
-        end
+      channel.stopping do
+        subs.each(&:unsubscribe).clear
       end
     end
 

docs/client-test.rb
@@ -19,7 +19,7 @@ end
 def statsd
   require 'statsd'
 
-  s = Statsd.new
+  s = Statsd::Client.new
 
   0.upto 10 do
     s.timing "statsd/test", 1000 * rand

docs/simple.conf
@@ -13,7 +13,7 @@
     :limit: 100
 
 :statistics:
-  :period: 120
+  :period: 5
   :tags:
     - "internal"
 
@@ -24,18 +24,18 @@
   - :type: "collectd"
   - :type: "json_line"
     :protocol: "tcp"
-  - :type: "riemann"
-    :protocol: "tcp"
+# - :type: "riemann"
+#   :protocol: "tcp"
   - :type: "carbon"
     :protocol: "tcp"
 
 :output:
    - :type: "kairosdb"
      :url: "http://localhost:8080"
-#  - :type: "riemann"
-#    :host: "riemann.example.com"
-#    :protocol: "tcp"
-#    :flush_period: 5
+   - :type: "riemann"
+     :host: "localhost"
+     :protocol: "tcp"
+     :flush_period: 5
    - :type: "log"
      :prefix: "0xdeadbeef"
 #  - :type: "kafka"

lib/ffwd/channel.rb
@@ -3,6 +3,21 @@ require 'set'
 # Defines containers which are limited in size.
 module FFWD
   class Channel
+    class Sub
+      attr_reader :channel, :block
+
+      def initialize channel, block
+        @channel = channel
+        @block = block
+      end
+
+      def unsubscribe
+        @channel.unsubscribe self
+      end
+    end
+
+    attr_reader :subs
+
     def initialize log, name
       @log = log
       @name = name
@@ -10,22 +25,23 @@ module FFWD
     end
 
     def <<(item)
-      @subs.each do |s|
+      @subs.each do |sub|
         begin
-          s.call item
+          sub.block.call item
         rescue => e
           @log.error "#{@name}: Subscription failed", e
         end
       end
     end
 
-    def subscribe(&cb)
-      @subs << cb
-      return cb
+    def subscribe(&block)
+      s = Sub.new(self, block)
+      @subs << s
+      return s
     end
 
-    def unsubscribe cb
-      @subs.delete cb
+    def unsubscribe sub
+      @subs.delete sub
     end
   end
 end

lib/ffwd/connection.rb
@@ -1,6 +1,12 @@
 require 'eventmachine'
 
 module FFWD
+  # Connections are used by input plugins in the protocol stack.
+  #
+  # The sole purpose this exists is to incorporate a datasink functionality
+  # in the EM::Connection.
+  #
+  # The datasink is used by tunnels to 'hook into' outgoing data.
   class Connection < EM::Connection
     def datasink= sink
       @datasink = sink

lib/ffwd/core.rb
@@ -7,6 +7,7 @@ require_relative 'core_interface'
 require_relative 'core_processor'
 require_relative 'core_reporter'
 require_relative 'debug'
+require_relative 'lifecycle'
 require_relative 'logging'
 require_relative 'plugin_channel'
 require_relative 'processor'
@@ -16,6 +17,7 @@ require_relative 'utils'
 
 module FFWD
   class Core
+    include FFWD::Lifecycle
     include FFWD::Logging
 
     def initialize plugins, opts={}
@@ -23,13 +25,14 @@ module FFWD
       @input_plugins = plugins[:input] || []
       @output_plugins = plugins[:output] || []
 
-      @statistics_opts = opts[:statistics] || {}
+      @statistics_opts = opts[:statistics]
       @debug_opts = opts[:debug]
       @core_opts = opts[:core] || {}
       @processor_opts = opts[:processor] || {}
 
       @output_channel = FFWD::PluginChannel.build 'output'
       @input_channel = FFWD::PluginChannel.build 'input'
+
       @system_channel = Channel.new log, "system_channel"
 
       memory_config = (@core_opts[:memory] || {})
@@ -40,20 +43,24 @@ module FFWD
         raise "memory limit must be non-negative number"
       end
 
+      @emitter = CoreEmitter.build @output_channel, @core_opts
+      @processor = CoreProcessor.build @input_channel, @emitter, @processor_opts
+
       @debug = nil
 
       if @debug_opts
         @debug = FFWD::Debug.setup @debug_opts
+        @debug.monitor "core.input", @input_channel, FFWD::Debug::Input
+        @debug.monitor "core.output", @output_channel, FFWD::Debug::Output
+        @debug.depend_on self
       end
-
-      @emitter = CoreEmitter.build @output_channel, @core_opts
-      @processor = CoreProcessor.build @input_channel, @emitter, @processor_opts
 
       # Configuration for statistics module.
       @statistics = nil
 
       if config = @statistics_opts
         @statistics = FFWD::Statistics.setup @emitter, @system_channel, config
+        @statistics.depend_on self
       end
 
       @interface = CoreInterface.new(
@@ -76,33 +83,39 @@ module FFWD
         reporters += @output_instances.select{|i| FFWD.is_reporter?(i)}
         @statistics.register "core", CoreReporter.new(reporters)
       end
+
+      # Make the core-related channels depend on core.
+      # They will then be orchestrated with core when it's being
+      # started/stopped.
+      @input_channel.depend_on self
+      @output_channel.depend_on self
     end
 
-    #
     # Main entry point.
     #
-    # Starts an EventMachine and runs the given set of plugins.
-    #
+    # Since all components are governed by the lifecycle of core, it should
+    # mostly be a matter of calling 'start'.
     def run
-      EM.run do
-        @input_channel.start
-        @output_channel.start
-
-        setup_memory_monitor
-
-        unless @statistics.nil?
-          @statistics.start
-        end
-
-        unless @debug.nil?
-          @debug.start
-          @debug.monitor "core.input", @input_channel, FFWD::Debug::Input
-          @debug.monitor "core.output", @output_channel, FFWD::Debug::Output
+      # What to do when we receive a shutdown signal?
+      shutdown_handler = proc do
+        # Hack to get out of trap context and into EM land.
+        EM.add_timer(0) do
+          log.info "Shutting down"
+          stop
+          EM.stop
         end
       end
 
-      @input_channel.stop
-      @output_channel.stop
+      EM.run do
+        Signal.trap("INT", &shutdown_handler)
+        Signal.trap("TERM", &shutdown_handler)
+
+        start
+        setup_memory_monitor
+      end
+
+      stopping do
+      end
     end
 
     private

lib/ffwd/core_processor.rb
@@ -23,25 +23,26 @@ module FFWD
       @processors = Hash[processors.map{|k, p| [k, p.call(@emitter)]}]
       @reporters = processors.select{|k, p| FFWD.is_reporter?(p)}.map{|k, p| p}
 
+      subs = []
+
       input.starting do
         @processors.each do |name, p|
           p.start
         end
 
-        metric_sub = input.metric_subscribe do |m|
+        subs << input.metric_subscribe do |m|
           process_metric m
         end
 
-        event_sub = input.event_subscribe do |e|
+        subs << input.event_subscribe do |e|
           process_event e
         end
+      end
 
-        input.stopping do
-          input.event_unsubscribe event_sub
-          input.metric_unsubscribe metric_sub
-          @processors.clear
-          @reporters.clear
-        end
+      input.stopping do
+        subs.each(&:unsubscribe).clear
+        @processors.clear
+        @reporters.clear
       end
     end
 

lib/ffwd/debug.rb
@@ -4,6 +4,8 @@ require 'eventmachine'
 require_relative 'logging'
 require_relative 'event'
 require_relative 'metric'
+require_relative 'retrier'
+require_relative 'lifecycle'
 
 require_relative 'debug/connection'
 require_relative 'debug/monitor_session'
@@ -43,18 +45,23 @@ module FFWD::Debug
 
   class TCP
     include FFWD::Logging
+    include FFWD::Lifecycle
 
-    def initialize host, port
+    def initialize host, port, rebind_timeout
       @clients = {}
       @sessions = {}
       @host = host
       @port = port
       @peer = "#{@host}:#{@port}"
-    end
 
-    def start
-      log.info "Binding on tcp://#{@peer}"
-      EM.start_server @host, @port, Connection, self
+      r = FFWD::Retrier.new(log, self, rebind_timeout) do |attempt|
+        EM.start_server @host, @port, Connection, self
+        log.info "Bind on tcp://#{@peer} (attempt #{attempt})"
+      end
+
+      r.error do |attempt, timeout, e|
+        log.error "Failed to bind tcp://#{@peer} (attempt #{attempt}), retry in #{timeout}s", e
+      end
     end
 
     def register_client peer, client
@@ -86,8 +93,6 @@ module FFWD::Debug
       @clients.each do |peer, client|
         session.register peer, client
       end
-
-      session.start
     end
 
     def unmonitor id
@@ -98,13 +103,16 @@ module FFWD::Debug
     end
   end
 
+  DEFAULT_REBIND_TIMEOUT = 10
+
   def self.setup opts={}
     host = opts[:host] || "localhost"
     port = opts[:port] || 9999
+    rebind_timeout = opts[:rebind_timeout] || DEFAULT_REBIND_TIMEOUT
     proto = FFWD.parse_protocol(opts[:protocol] || "tcp")
 
     if proto == FFWD::TCP
-      return TCP.new host, port
+      return TCP.new host, port, rebind_timeout
     end
 
     throw Exception.new("Unsupported protocol '#{proto}'")

commit 59005cbb683d99a0ea57c149c6447674eed5d7b2

Fixed missing in channel ID in debug output
* Regression introduced in 129d983012dc4af27748d3b8b51b90ffb16ddb48

lib/ffwd/debug/monitor_session.rb
@@ -20,6 +20,7 @@ module FFWD::Debug
     attr_reader :id
 
     def initialize id, channel, type
+      @id = id
       @type = type
       @clients = {}
 