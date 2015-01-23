5763942b745f54dab9175bea0f188e996137a415
Mehrdad Hassan Abadi <mehrdad@spotify.com>
2015-01-23 09:59:11 +0100

    [WIP] initial basic functionalities of Kafka output plugin

--- ffwd-java/modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaPluginSink.java
+++ ffwd-java/modules/kafka/src/main/java/com/spotify/ffwd/kafka/KafkaPluginSink.java
@@ -1,34 +1,56 @@
 package com.spotify.ffwd.kafka;
 
-import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+
 import com.spotify.ffwd.model.Event;
 import com.spotify.ffwd.model.Metric;
-import com.spotify.ffwd.output.PluginSink;
+import com.spotify.ffwd.output.AbstractPluginSink;
 
-import eu.toolchain.async.AsyncFramework;
-import eu.toolchain.async.AsyncFuture;
+public class KafkaPluginSink extends AbstractPluginSink {
 
-public class KafkaPluginSink implements PluginSink {
-    @Inject
-    private AsyncFramework async;
+    private final Producer<String, String> producer;
 
-    @Override
-    public AsyncFuture<Void> sendEvent(Event event) {
-        return async.resolved(null);
+    private final String topic;
+
+    public KafkaPluginSink(final Producer<String, String> producer, final String topic, final Integer flushInterval) {
+        super(flushInterval);
+        this.producer = producer;
+        this.topic = topic;
     }
 
     @Override
-    public AsyncFuture<Void> sendMetric(Metric metric) {
-        return async.resolved(null);
+    public void doSendEvent(final Event event) {
+        producer.send(createMessage(topic, event));
     }
 
     @Override
-    public AsyncFuture<Void> start() {
-        return async.resolved(null);
+    public void doSendMetric(final Metric metric) {
+        producer.send(createMessage(topic, metric));
     }
 
     @Override
-    public AsyncFuture<Void> stop() {
-        return async.resolved(null);
+    public void doSendAllEvents(final List<Event> events) {
+        producer.send(createMessages(topic, events));
+    }
+
+    @Override
+    public void doSendAllMetrics(final List<Metric> metrics) {
+        producer.send(createMessages(topic, metrics));
+    }
+
+    private <T> List<KeyedMessage<String, String>> createMessages(final String topic, final List<T> elements) {
+        final List<KeyedMessage<String, String>> messages = new ArrayList<KeyedMessage<String,String>>();
+        for (final T e : elements) {
+            messages.add(createMessage(topic, e));
+        }
+        return messages;
+    }
+
+    private <T> KeyedMessage<String, String> createMessage(final String topic, final T element) {
+        return new KeyedMessage<String, String>(topic, element.toString());
     }
 }
